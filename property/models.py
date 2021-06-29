28008e9cb6fc0a5f4b0c417e3d299ff636a06dd4
Araime <alvonsa09121985@gmail.com>
2021-06-29 16:44:55 +0400

    Add field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+STATUS_CHOICES = (
+    (True, 'новостройка'),
+    (False, 'старое здание'),
+    (None, 'не заполнено'),
+)
+
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
@@ -9,10 +15,8 @@ class Flat(models.Model):
         'Когда создано объявление',
         default=timezone.now,
         db_index=True)
-
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
-
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
@@ -29,7 +33,6 @@ class Flat(models.Model):
         'Этаж',
         max_length=3,
         help_text='Первый этаж, последний этаж, пятый этаж')
-
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
         db_index=True)
@@ -38,7 +41,6 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
@@ -46,6 +48,7 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
+    new_building = models.NullBooleanField('Новостройка', choices=STATUS_CHOICES, db_index=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
